2bcc78ae1e6b9f6d69ba3d2a108d5078bd9c1bce
Rayhaan Ugharadar <Rayhaan@Mohameds-MacBook-Pro.local>
2022-02-08 17:20:35 +0000

    installed react-icons and styled page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,9 @@
-
+import{SiReddit} from "react-icons/si"
 
 export function Footer() {
     return (
         <div className="footer">
-            <h2>rayddit</h2>
+            <h2 id="footer-title"><span className="logo"><SiReddit /></span>  rayddit</h2>
             <h5>by Rayhaan Ugharadar</h5>
             <h5>2022</h5>
         </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,10 @@
 import React from "react";
+import{IoIosCafe} from "react-icons/io"
+import{SiReddit} from "react-icons/si"
 
 export function Header() {
     return <div className="header">
-        <h1 id="main-header">rayddit </h1>
+        <h1 id="main-header"><span className="logo"><SiReddit /></span>  rayddit </h1>
+        <h6 id="sub-heading">A Rayact app</h6>
     </div>
 };

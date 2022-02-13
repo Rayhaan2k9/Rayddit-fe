47688288fba109b41717de55270128504d672dba
Rayhaan Ugharadar <Rayhaan@Mohameds-MacBook-Pro.local>
2022-02-13 15:22:03 +0000

    added React logo in footer and further app styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,11 @@
 import{SiReddit} from "react-icons/si"
+import {FaReact} from "react-icons/fa"
 
 export function Footer() {
     return (
         <div className="footer">
             <h2 id="footer-title"><span className="logo"><SiReddit /></span>  rayddit</h2>
-            <h5>A Rayact App <br></br>by Rayhaan Ugharadar</h5>
+            <h5>A <span id="react-logo"><FaReact /> </span>Rayact App <br></br>by Rayhaan Ugharadar</h5>
             <h5>2022</h5>
         </div>
     )

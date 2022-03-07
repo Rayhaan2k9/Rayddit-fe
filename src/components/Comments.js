f087e64cdd0364cf05ceba5ba2a86ec8cebdfa6c
Rayhaan Ugharadar <Rayhaan@Mohameds-MacBook-Pro.local>
2022-03-07 19:54:52 +0000

    removed alert and implemented deleting message loading text

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -9,6 +9,8 @@ export function Comments() {
 const [comments, setComments] = useState([]);
 const [commentBody, setCommentBody] = useState('')
 const {loggedInUser, isLoggedIn} = useContext(UserContext)
+const [loginPrompt, setLoginPrompt] = useState(null)
+const [deleting, setDeleting] = useState(false)
 
 const handleTextChange = (event) => {
 setCommentBody(event.target.value)
@@ -20,38 +22,50 @@ const handlePostSubmit = (event) => {
     setCommentBody('')
     postComment(article_id, loggedInUser.username, commentBody)
     } else {
-        alert('Please log in to post a comment!')
+        event.preventDefault();
+        setCommentBody('')
+        setLoginPrompt('Please log in to post a comment!')
     } 
 }
 
 const handleDelete = (value) => () => {
-    deleteComment(value)
-    alert('comment deleted')
+    setDeleting(true)
+     deleteComment(value).then(() => {
+    setTimeout(() => {     
+     setDeleting(false) 
+    }, 2000);  
+    });  
+    
+    
+    
 }
 
 
 useEffect(() => {
     getComments(article_id)
     .then((commentsFromApi) => {
-        setComments(commentsFromApi)
+        setComments([...commentsFromApi])
     })
 }, [comments, article_id])
 
     return (
         <div>
-            <ul>
-                {comments.map((comment) => {
-                    return <li key={comment.comment_id} className="main-article"><h4>{comment.author}</h4>
-                    <h5>Posted on {formatDate(comment.created_at)}</h5>
-                    <p>{comment.body}</p>
-                    {loggedInUser.username === comment.author ? <button onClick={handleDelete(comment.comment_id)}>Delete</button> : null}</li>
-                })}
-                
-            </ul>
-            <form onSubmit={handlePostSubmit}>
+            <p className='prompt'>{loginPrompt}</p>
+            <form id='comment-form' onSubmit={handlePostSubmit}>
                 <textarea placeholder='Type your comment here' onChange={handleTextChange}rows='4' cols='50' value={commentBody} required/>
                 <button>Post</button>
             </form>
+            <ul id='comment-list'>
+                {comments.map((comment) => {
+                    return <li key={comment.comment_id} className="main-article"><h4>{comment.author}</h4>
+                    <h5>Posted on {formatDate(comment.created_at)}</h5>
+                   <p>{comment.body}</p> 
+                   
+                    {loggedInUser.username === comment.author && !deleting ? <button onClick={handleDelete(comment.comment_id)}>{deleting ? 'Deleting comment...' : 'Delete'}</button> : loggedInUser.username === comment.author && deleting ? <p className='prompt'>Deleting comment...</p> : null}</li>
+                })}
+                
+            </ul>
+            
         </div>
     )
 }

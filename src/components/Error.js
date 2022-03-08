df0add982b96dd166494e63a3f822d202d80eec3
Rayhaan Ugharadar <Rayhaan@Mohameds-MacBook-Pro.local>
2022-03-08 13:17:14 +0000

    added link to articles on error component

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,12 +1,17 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 export const Error = (props) => {
-console.log(props.error)
+
 
     return (
+        <div className="single-article">
         <div id="error-container">
-            <h1>{props.error.status}</h1>
-            <h1>{props.error.data.message}</h1>
+            <h1>Error {props.error.status}</h1>
+            <h2>{props.error.data.message}</h2>
+            <Link to='/articles'>
+                <h4>Click here to go back to articles</h4>
+                </Link>
+        </div>
         </div>
     )
 }

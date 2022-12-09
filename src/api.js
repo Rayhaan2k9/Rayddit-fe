bb0ea2c956256de6936eb792539b89b10eef2441
Rayhaan Ugharadar <rayhaanugharadar@Rayhaans-MacBook-Pro.local>
2022-12-09 14:39:17 +0000

    update api from heroku to cyclic

--- src/api.js
+++ src/api.js
@@ -1,71 +1,72 @@
 import axios from "axios";
-const dayjs = require("dayjs")
+const dayjs = require("dayjs");
 
 const raydditApi = axios.create({
-    baseURL: 'https://rayddit.herokuapp.com/api'
-})
+  baseURL: "https://calm-plum-shorts.cyclic.app/api",
+});
 
 export const getUsers = () => {
-    return raydditApi.get(`/users`).then((res) => {
-        return res.data.users
-    })
-}
+  return raydditApi.get(`/users`).then((res) => {
+    return res.data.users;
+  });
+};
 
 export const getArticles = (sortQry, topicQry) => {
-    return raydditApi.get(`/articles`, {params: {topic: topicQry, sort_by: sortQry}})
+  return raydditApi
+    .get(`/articles`, { params: { topic: topicQry, sort_by: sortQry } })
     .then((res) => {
-        return res.data.articles
-    })
-}
+      return res.data.articles;
+    });
+};
 
 export const getTopics = () => {
-    return raydditApi.get(`/topics`)
-    .then ((res) => {
-        return res.data.topics
-    })
-}
+  return raydditApi.get(`/topics`).then((res) => {
+    return res.data.topics;
+  });
+};
 
 export const getSingleArticle = (article_id) => {
-    return raydditApi.get(`/articles/${article_id}`)
-    .then((res) => {
-        return res.data.article
-    })
-}
+  return raydditApi.get(`/articles/${article_id}`).then((res) => {
+    return res.data.article;
+  });
+};
 
 export const voteArticle = (article_id) => {
-    return raydditApi.patch(`/articles/${article_id}`, {inc_votes : 1})
+  return raydditApi
+    .patch(`/articles/${article_id}`, { inc_votes: 1 })
     .then((res) => {
-        return res.data.article
-    })
-}
+      return res.data.article;
+    });
+};
 
 export const undoVote = (article_id) => {
-    return raydditApi.patch(`/articles/${article_id}`, {inc_votes: -1})
+  return raydditApi
+    .patch(`/articles/${article_id}`, { inc_votes: -1 })
     .then((res) => {
-        return res.data.article
-    })
-}
+      return res.data.article;
+    });
+};
 
 export const getComments = (article_id) => {
-    return raydditApi.get(`/articles/${article_id}/comments`)
-    .then((res) => {
-        return res.data.comments
-    })
-}
+  return raydditApi.get(`/articles/${article_id}/comments`).then((res) => {
+    return res.data.comments;
+  });
+};
 
 export const postComment = (article_id, author, body) => {
-    return raydditApi.post(`/articles/${article_id}/comments`, {username: author, body: body})
+  return raydditApi
+    .post(`/articles/${article_id}/comments`, { username: author, body: body })
     .then((res) => {
-        return res.data.comment
-    })
-}
+      return res.data.comment;
+    });
+};
 
 export const deleteComment = (comment_id) => {
- raydditApi.delete(`/comments/${comment_id}`)
-}
+  raydditApi.delete(`/comments/${comment_id}`);
+};
 
 export const formatDate = (date) => {
-    if (date) {
-        return dayjs(date).$d.toString().substring(4, 15);
-    }
-}
+  if (date) {
+    return dayjs(date).$d.toString().substring(4, 15);
+  }
+};
